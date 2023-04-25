f75b413d509976c4e890f0aeedfc37592eaba933
Huy Tran <huytran21007@gmail.com>
2023-04-25 08:02:33 +0700

    🐛 FIX: Fix login page loading lag

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -13,14 +13,26 @@ import { LOCAL_SERVICE } from "../../core/services/localServ";
 import { checkAllInfo } from "../../core/utils/checkLogin";
 import { useDispatch } from "react-redux";
 import { spinnerActions } from "../../core/redux/slice/spinnerSlice";
+import { useState } from "react";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    let timeOutId;
+    dispatch(spinnerActions.setLoadingOn());
     if (LOCAL_SERVICE.user.get()) {
       navigate("/");
     }
+
+    timeOutId = setTimeout(() => {
+      setLoading(false);
+      dispatch(spinnerActions.setLoadingOff());
+    }, 1500);
+
+    return () => clearTimeout(timeOutId);
   }, []);
 
   const handleFinish = (values, buttonRef) => {
@@ -52,7 +64,7 @@ const LoginPage = () => {
           } else {
             LOCAL_SERVICE.user.set({ ...res, tasks: [] }, res.role);
           }
-        }, 2500);
+        }, 4500);
       })
       .catch((error) => {
         console.log("error");
@@ -62,72 +74,35 @@ const LoginPage = () => {
 
   const renderPage = () => {
     return (
-      <PageWrapper className="page-login h-full">
-        <Container className="h-full">
-          <div className="wrapper flex items-center justify-center h-full">
-            <Space
-              className="form-wrapper bg-white rounded-[15px] p-7 max-w-[500px] w-full"
-              align="center"
-              direction="vertical"
-              size={20}
-            >
-              <div className="form-header text-center w-full">
-                <Link to="/" className="pb-8 flex items-center justify-center">
-                  <img src={logoPage} alt="logo-page" className="logo" />
-                </Link>
-                <h3 className="form-title border-t border-solid border-[#EBF1FF] pt-8 text-xl font-semibold mb-0">
-                  Login
-                </h3>
-              </div>
-              <div className="form-body">
-                <LoginForm handleFinish={handleFinish} />
-              </div>
-              {/* <div className="sign-up-txt mb-0 text-center">
-                <p className="txt">
-                  Not A Member ?{" "}
-                  <Link to="/" className="text-blue-ribbon-500 ml-1">
-                    Create An Account
+      !loading && (
+        <PageWrapper className="page-login h-full">
+          <Container className="h-full">
+            <div className="wrapper flex items-center justify-center h-full">
+              <Space
+                className="form-wrapper bg-white rounded-[15px] p-7 max-w-[500px] w-full"
+                align="center"
+                direction="vertical"
+                size={20}
+              >
+                <div className="form-header text-center w-full">
+                  <Link
+                    to="/"
+                    className="pb-8 flex items-center justify-center"
+                  >
+                    <img src={logoPage} alt="logo-page" className="logo" />
                   </Link>
-                </p>
-              </div> */}
-              {/* <div className="social-link">
-                <ul className="account-social-link flex justify-center items-center gap-5">
-                  <li>
-                    <Link
-                      to="https://www.google.com/"
-                      target="_blank"
-                      className="w-12 h-12 rounded-full  bg-[#EBF1FF] flex items-center justify-center"
-                    >
-                      <AiOutlineGoogle
-                        className="text-blue-ribbon-500"
-                        size={20}
-                      />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="https://www.facebook.com/"
-                      target="_blank"
-                      className="w-12 h-12 rounded-full text-blue-ribbon-500 bg-[#EBF1FF] flex items-center justify-center"
-                    >
-                      <FaFacebookF className="text-blue-ribbon-500" size={16} />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="https://www.twitter.com/"
-                      target="_blank"
-                      className="w-12 h-12 rounded-full text-blue-ribbon-500 bg-[#EBF1FF] flex items-center justify-center"
-                    >
-                      <FaTwitter className="text-blue-ribbon-500" size={16} />
-                    </Link>
-                  </li>
-                </ul>
-              </div> */}
-            </Space>
-          </div>
-        </Container>
-      </PageWrapper>
+                  <h3 className="form-title border-t border-solid border-[#EBF1FF] pt-8 text-xl font-semibold mb-0">
+                    Login
+                  </h3>
+                </div>
+                <div className="form-body">
+                  <LoginForm handleFinish={handleFinish} />
+                </div>
+              </Space>
+            </div>
+          </Container>
+        </PageWrapper>
+      )
     );
   };
 
