1afd201c4c56fafaa6e27c4dbd85b73dbb30535d
Huy Tran <huytran21007@gmail.com>
2023-05-08 09:19:00 +0700

    Add button "Add admin"

--- src/Pages/AdminManagement/AdminManagePage.jsx
+++ src/Pages/AdminManagement/AdminManagePage.jsx
@@ -4,6 +4,8 @@ import Header from "../../core/Components/Header/Header";
 import { LOCAL_SERVICE } from "../../core/services/localServ";
 import { useNavigate } from "react-router-dom";
 import AdminManageTable from "./AdminManageTable";
+import { Button } from "antd";
+import { UserAddOutlined } from "@ant-design/icons";
 const AdminManagePage = () => {
   let navigate = useNavigate();
 
@@ -13,12 +15,24 @@ const AdminManagePage = () => {
     }
   }, []);
 
+  const tableHeader = (
+    <div className="flex justify-between items-center">
+      <h3>Admin Management</h3>
+      <Button
+        className="inline-flex items-center justify-center bg-indigo-500/100 p-5 mb-5 text-white text-base capitalize"
+        onClick={() => navigate("/master/admin/add-admin")}
+      >
+        <UserAddOutlined />
+        Add admin
+      </Button>
+    </div>
+  );
   return (
     <>
       <Header />
       <SectionWrapper
         sectionClass={"master"}
-        title={"Admin Management"}
+        title={tableHeader}
         content={<AdminManageTable />}
       />
     </>
